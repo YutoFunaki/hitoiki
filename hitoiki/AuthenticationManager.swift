6db06c479cff6cbc2f50f655ce479cb466082217
YutoFunaki <kaizuka0629@gmail.com>
2024-07-21 21:02:46 +0900

    認証機能完成、ログイン画面の背景色がグレーなのが気になる

---
+++ hitoiki/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  hitoiki
+//
+//  Created by 船木勇斗 on 2024/07/21.
+//
+
+import Foundation
---
+++ hitoiki/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  hitoiki
+//
+//  Created by 船木勇斗 on 2024/07/21.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
