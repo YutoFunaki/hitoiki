a66f1d73650f26c0d138dfc71a8a191c86ee11aa
YutoFunaki <kaizuka0629@gmail.com>
2024-07-21 19:37:34 +0900

    Initial Commit

---
+++ hitoiki/hitoikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hitoikiApp.swift
+//  hitoiki
+//
+//  Created by 船木勇斗 on 2024/07/21.
+//
+
+import SwiftUI
+
+@main
+struct hitoikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
