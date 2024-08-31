1fb1be8d5fcfb4c6d7c36ef5e3c6c2fbfb8c67fc
YutoFunaki <kaizuka0629@gmail.com>
2024-08-31 13:58:57 +0900

    色々追加

---
+++ hitoiki/Views/ArticleEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleEditView.swift
+//  hitoiki
+//
+//  Created by 船木勇斗 on 2024/08/31.
+//
+
+import SwiftUI
+
+struct ArticleEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleEditView()
+}
---
+++ hitoiki/Views/DeveloperArticleManagementView.swift
@@ -0,0 +1,18 @@
+//
+//  DeveloperArticleManagementView.swift
+//  hitoiki
+//
+//  Created by 船木勇斗 on 2024/08/25.
+//
+
+import SwiftUI
+
+struct DeveloperArticleManagementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeveloperArticleManagementView()
+}
